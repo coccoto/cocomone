86eccea2ad90d31e8d83b68b27d6de796d64b95f
coccoto <soprano.lune@gmail.com>
2020-02-05 00:39:06 +0900

    2020-02-05 00:39

---
+++ app/views/data/head.php
@@ -0,0 +1,8 @@
+<meta charset='utf-8'>
+<meta name='author' content=''>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<title><?php echo $this->property['title'] ?></title>
+<link rel='icon' type='image/png' href=''>
+<link rel='apple-touch-icon' href=''>
+<?php $this->html->useStyle() ?>
+<?php $this->html->useScript() ?>
