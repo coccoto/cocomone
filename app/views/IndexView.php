60b25c9dca0afe10ade2690eae35078e1c5f401e
cocoto <marilun.lune@gmail.com>
2020-02-05 00:39:06 +0900

    2020-02-05 00:39

---
+++ app/views/IndexView.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\views;
+
+final class IndexView extends CommonView {
+
+    public function __construct() {
+
+        parent::__construct();
+    }
+
+    private function indexMethod() {
+    }
+}
---
+++ app/views/data/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once 'head.php' ?>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
